66daa6252de88ca82089805765e897fb34b62ea0
silva.alan <silva.alan@cwi.com.br>
2020-07-03 12:15:43 -0300

    Improving ExpandableTextView component.

--- app/src/main/java/com/example/archv1/data/util/ExpandedTextView.kt
+++ app/src/main/java/com/example/archv1/data/util/ExpandedTextView.kt
@@ -1,123 +1,96 @@
 package com.example.archv1.data.util
 
 import android.content.Context
-import android.text.*
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.example.archv1.R
 
 class ExpandedTextView(context: Context, attrs: AttributeSet?) :
     androidx.appcompat.widget.AppCompatTextView(context, attrs) {
 
-    private var fullText: CharSequence? = null
-    private var trimmedText: CharSequence? = null
-    private var isExpanded = false
     private val DEFAULT_TRIM_LENGTH = 120
     private val SHOW_MORE = "Ver mais"
     private val SHOW_LESS = "Ver menos"
-    private val spannableStringMore : SpannableString by lazy {
-        val textToShow  = currentText ?: ""
-        val link = if (isExpanded) SHOW_LESS else SHOW_MORE
-        val startLink = textToShow.length - link.length
-        val spannableString = SpannableString(textToShow)
-        val clickableSpan = object : ClickableSpan() {
-            override fun onClick(view: View) {
-                Log.d("03072020", "click more")
-                isExpanded = isExpanded.not()
-                setText(spannableStringLess, BufferType.SPANNABLE)
-            }
-
-            override fun updateDrawState(ds: TextPaint) {
-                super.updateDrawState(ds)
-                ds.isUnderlineText = false
-            }
-        }
-        spannableString.setSpan(clickableSpan, startLink, startLink + link.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableString.setSpan(ForegroundColorSpan(ContextCompat.getColor(context, R.color.blue_500)), startLink, startLink + link.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableString.setSpan(StyleSpan(android.graphics.Typeface.BOLD), startLink, textToShow.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        movementMethod = LinkMovementMethod.getInstance()
-        spannableString
-    }
-
-    private val spannableStringLess : SpannableString by lazy {
-        val textToShow  = currentText ?: ""
-        val link = if (isExpanded) SHOW_LESS else SHOW_MORE
-        val startLink = textToShow.length - link.length
-        val spannableString = SpannableString(textToShow)
-        val clickableSpan = object : ClickableSpan() {
-            override fun onClick(view: View) {
-                Log.d("03072020", "click less")
-                isExpanded = isExpanded.not()
-                setText(spannableStringMore, BufferType.SPANNABLE)
-            }
-
-            override fun updateDrawState(ds: TextPaint) {
-                super.updateDrawState(ds)
-                ds.isUnderlineText = false
-            }
-        }
-        spannableString.setSpan(clickableSpan, startLink, startLink + link.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableString.setSpan(ForegroundColorSpan(ContextCompat.getColor(context, R.color.blue_500)), startLink, startLink + link.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableString.setSpan(StyleSpan(android.graphics.Typeface.BOLD), startLink, textToShow.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        movementMethod = LinkMovementMethod.getInstance()
-        spannableString
-    }
-
-    private val currentText: CharSequence?
+    private var fullText: CharSequence
+    private var trimmedText: CharSequence
+    private var isExpanded = false
+    private val spannableStringMore: SpannableString by lazy { buildSpannableString() }
+    private val spannableStringLess: SpannableString by lazy { buildSpannableString() }
+    private val currentText: CharSequence
         get() = if (isExpanded) fullText else trimmedText
 
     init {
-
         fullText = getFullText(text)
         trimmedText = getTrimmedText(text)
-//        setText()
-        setText(spannableStringLess, BufferType.SPANNABLE)
-
-//        setOnClickListener {
-//            isExpanded = isExpanded.not()
-//            setText()
-//        }
+        setTextView()
     }
 
-    private fun setText() {
-        currentText?.let { textToShow ->
-            val link = if(isExpanded) SHOW_LESS else SHOW_MORE
-            val startLink = textToShow.length - link.length
-            with(SpannableString(textToShow)) {
-                val clickableSpan = object : ClickableSpan() {
-                    override fun onClick(view: View) {
-                        Log.d("03072020", "click")
-                    }
-                    override fun updateDrawState(ds: TextPaint) {
-                        super.updateDrawState(ds)
-                        ds.isUnderlineText = false
-                    }
-                }
-                setSpan(clickableSpan, startLink, startLink + link.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-                setSpan(ForegroundColorSpan(ContextCompat.getColor(context, R.color.blue_500)), startLink, startLink + link.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                setSpan(StyleSpan(android.graphics.Typeface.BOLD), startLink, textToShow.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                movementMethod = LinkMovementMethod.getInstance()
-                setText(this, BufferType.SPANNABLE)
-            }
-        }
+    private fun setTextView() {
+        val spannableString = if (isExpanded) spannableStringLess else spannableStringMore
+        setText(spannableString, BufferType.SPANNABLE)
+        isExpanded = isExpanded.not()
     }
 
-    private fun getTrimmedText(text: CharSequence) = if (text.length > DEFAULT_TRIM_LENGTH) {
+    private fun isExpandable(text: CharSequence) = text.length > DEFAULT_TRIM_LENGTH
+
+    private fun getTrimmedText(text: CharSequence) = if (isExpandable(text)) {
         SpannableStringBuilder(text, 0, DEFAULT_TRIM_LENGTH).append("\n$SHOW_MORE")
     } else {
         text
     }
 
-    private fun getFullText(text: CharSequence) = if (text.length > DEFAULT_TRIM_LENGTH) {
+    private fun getFullText(text: CharSequence) = if (isExpandable(text)) {
         SpannableStringBuilder(text).append("\n$SHOW_LESS")
     } else {
         text
+    }
+
+    private fun buildSpannableString(): SpannableString {
+        return SpannableString(currentText).apply {
+            if (isExpandable(currentText)) {
+                val link = if (isExpanded) SHOW_LESS else SHOW_MORE
+                val startLink = currentText.length - link.length
+                val clickableSpan = object : ClickableSpan() {
+                    override fun onClick(view: View) {
+                        setTextView()
+                    }
+
+                    override fun updateDrawState(ds: TextPaint) {
+                        super.updateDrawState(ds)
+                        ds.isUnderlineText = false
+                    }
+                }
+                setSpan(
+                    clickableSpan,
+                    startLink,
+                    startLink + link.length,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                setSpan(
+                    ForegroundColorSpan(ContextCompat.getColor(context, R.color.blue_500)),
+                    startLink,
+                    startLink + link.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                setSpan(
+                    StyleSpan(Typeface.BOLD),
+                    startLink,
+                    currentText.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        }
     }
 }
